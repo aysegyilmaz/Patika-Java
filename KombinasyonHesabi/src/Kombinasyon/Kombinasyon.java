82f0244003639481334a951cd537deed5e29b274
aysegyilmaz <106971265+aysegyilmaz@users.noreply.github.com>
2023-08-06 18:49:03 +0300

    Add files via upload

---
+++ KombinasyonHesabi/src/Kombinasyon/Kombinasyon.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+
+public class Kombinasyon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n sayısını Giriniz: ");
+        int n = input.nextInt();
+
+        System.out.print("r sayısını giriniz: ");
+        int r = input.nextInt();
+
+        int total = 1;
+
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+
+        }
+        int res = 1;
+        for (int i = 1; i <= r; i++) {
+            res *= i;
+
+        }
+        int a = 1;
+        for (int i = 1; i <= (n - r); i++) {
+            a *= i;
+
+        }
+        int com = (total / (res * a));
+        System.out.println(n + " sayısının "+ r + " ile kombinasyonu = " + com);
+
+    }
+}
